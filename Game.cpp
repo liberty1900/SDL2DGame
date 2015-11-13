d1e23f808f25a7f5090822b95a6b59246c46c92f
liberty1900 <rzlove2014@outlook.com>
2015-11-14 00:50:50 +0800

    adjust to TextureManager

--- Game.cpp
+++ Game.cpp
@@ -5,10 +5,8 @@ Game::Game(void)
 {
 	pWindow=NULL;
 	pRenderer=NULL;
-	pTexture=NULL;
+	currentFrame=0;
 	running=true;
-	srcRect.x=srcRect.y=0;
-	dstRect.x=dstRect.y=0;
 }
 
 
@@ -19,15 +17,8 @@ Game::~Game(void)
 bool Game::init(const char* title,int x,int y,int w,int h,Uint32 flags){
 	if(SDL_Init(SDL_INIT_EVERYTHING)==0){
 		pWindow=SDL_CreateWindow(title,x,y,w,h,flags);
-		//必须使用SDL2_image-2.0.0中的各种dll
-		SDL_Surface* pSurface=IMG_Load("images/sprite.png");
-		pRenderer=SDL_CreateRenderer(pWindow,-1,0);
-		pTexture=SDL_CreateTextureFromSurface(pRenderer,pSurface);
-		SDL_FreeSurface(pSurface);
-		dstRect.w=srcRect.w=100;
-		dstRect.h=srcRect.h=150;
-		dstRect.x=(900-dstRect.w)/2;
-		dstRect.y=(675-dstRect.h)/2;
+		pRenderer=SDL_CreateRenderer(pWindow,-1,0);	
+		textureManager.load("images/sprite.png","player",pRenderer);//create texture* textureMap["player"]
 		return true;
 	}else{
 		return false;
@@ -50,21 +41,17 @@ void Game::handleEvents(){
 }
 
 void Game::update(){
-	if(srcRect.x!=300){
-		srcRect.x+=100;
-	}else{
-		srcRect.x=0;
-	}
+	currentFrame++;
 }
 
 void Game::clean(){
-	SDL_DestroyTexture(pTexture);
-	SDL_DestroyRenderer(pRenderer);
-	SDL_DestroyWindow(pWindow);
+	//SDL_DestroyTexture();
+	//SDL_DestroyRenderer(pRenderer);
+	//SDL_DestroyWindow(pWindow);
 }
 void Game::render(){
 	SDL_SetRenderDrawColor(pRenderer,0,80,0,255);
 	SDL_RenderClear(pRenderer);//clear the window to red
-	SDL_RenderCopy(pRenderer,pTexture,&srcRect,&dstRect);//using the renderer to draw the texture
+	textureManager.drawFrame(pRenderer,"player",currentFrame,100,150,0,0,SDL_FLIP_NONE);
 	SDL_RenderPresent(pRenderer);
 }
