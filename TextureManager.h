b4404ff62e19d5f00a8cea90552286e7d98e97db
liberty1900 <rzlove2014@outlook.com>
2015-11-14 00:52:39 +0800

    Create TextureManager.h
    Manager all the texture

---
+++ TextureManager.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <Map>
+#include "SDL.h"
+class TextureManager
+{
+public:
+	TextureManager(void);
+
+	bool load(std::string filename,std::string id,SDL_Renderer* renderer);
+	//using renderer to simply draw the original image to the screen 
+	void draw(SDL_Renderer* pRenderer,std::string id,int dstX,int dstY,int srcWidth,int srcHeight,SDL_RendererFlip flip);
+	//draw a specific part(flame) of the image to the screen
+	//SUPPOSING the sprite sheet only has 1 row and 4 frames 
+	void drawFrame(SDL_Renderer* pRenderer,std::string id,int currentFlame,int srcWidth,int srcHeight,int dstX,int dstY,SDL_RendererFlip flip);
+
+	~TextureManager(void);
+private:
+	std::map<std::string,SDL_Texture*> textureMap;
+};
